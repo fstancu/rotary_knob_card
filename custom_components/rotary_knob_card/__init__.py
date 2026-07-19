7dd5cad342b785b40be2c75c9f92df35125dad75
Florin Stancu <fstancu@gmail.com>
2026-07-19 13:06:55 +0300

    Restructure for HACS Dashboard compatibility

--- custom_components/rotary_knob_card/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-_LOGGER = logging.getLogger(__name__)
-
-async def async_setup(homeassistant):
-    """Set up the rotary knob card component."""
-    _LOGGER.info("Rotary Knob Card integration successfully loaded")
-    return True
